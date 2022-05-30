59237dec6ad90d14d543987c602ca17b3120a722
Samuel HENTRICS <samuel.hentrics@gmail.com>
2022-05-30 14:31:02 +0200

    Modification de chifoumi.pro et ajout de connexion/deco de la bdd

--- database.cpp
+++ database.cpp
@@ -4,3 +4,20 @@ Database::Database()
 {
 
 }
+
+bool Database::openDatabase()
+{
+    mydb = QSqlDatabase::addDatabase(CONNECT_TYPE);
+    mydb.setDatabaseName(DATABASE_NAME);
+    if(mydb.open())
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void Database::closeDatabase()
+{
+    mydb.close();
+}
--- database.h
+++ database.h
@@ -1,11 +1,21 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <QSqlDatabase>
+
+#define DATABASE_NAME "bdd_chifoumi"
+#define CONNECT_TYPE "QODBC"
 
 class Database
 {
 public:
     Database();
+    bool openDatabase();
+    void closeDatabase();
+
+private:
+    QSqlDatabase mydb;
+
 };
 
 #endif // DATABASE_H

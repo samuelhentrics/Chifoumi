4f6b743bd92ac658672c4f81ddf5630c167c02fe
Samuel HENTRICS <samuel.hentrics@gmail.com>
2022-06-07 15:20:07 +0200

    maj aProposDe

--- chifoumivue.cpp
+++ chifoumivue.cpp
@@ -291,7 +291,7 @@ void ChifoumiVue::jouerPartie(Chifoumi::UnCoup coup){
 void ChifoumiVue::aProposDe(){
     QMessageBox* mBoxInfo = new QMessageBox();
     mBoxInfo->setWindowTitle("A propos de cette application");
-    mBoxInfo->setText("Version 8.0.\n"
+    mBoxInfo->setText("Version 9.0.\n"
                       "Dernière modification le : 07/06/2022.\n"
                       "Crée par Samuel HENTRICS LOISTINE, Cédric ETCHEPARE, Ahmed FAKHFAKH");
     mBoxInfo->show();
